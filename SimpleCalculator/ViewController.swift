6bd11bbe59a79286020920e9cf88bb9c3b45eb14
ismailpalali <ismail.palali.pp@gmail.com>
2021-12-09 21:56:54 +0300

    first commit

--- SimpleCalculator/ViewController.swift
+++ SimpleCalculator/ViewController.swift
@@ -9,11 +9,71 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var firstText: UITextField!
+    
+    @IBOutlet weak var secondText: UITextField!
+    
+    
+    @IBOutlet weak var resultText: UILabel!
+    
+    var result = 0
+    //
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    
+    @IBAction func sumButton(_ sender: Any) {
+        
+        if let firstNumber = Int(firstText.text!){
+            if let secondNumber = Int(secondText.text!){
+            
+                result = firstNumber + secondNumber
+                
+                resultText.text = String(result)
+            }
+        }
+        
+    }
+    
+    
+    @IBAction func minusButton(_ sender: Any) {
+        if let firstNumber = Int(firstText.text!){
+            if let secondNumber = Int(secondText.text!){
+            
+                result = firstNumber - secondNumber
+                
+                resultText.text = String(result)
+            }
+        }
+    }
+    
+    @IBAction func multilpyButton(_ sender: Any) {
+        
+        if let firstNumber = Int(firstText.text!){
+            if let secondNumber = Int(secondText.text!){
+            
+                result = firstNumber * secondNumber
+                
+                resultText.text = String(result)
+            }
+        }
+    }
+    
+    @IBAction func divideButton(_ sender: Any) {
+        
+        if let firstNumber = Int(firstText.text!){
+            if let secondNumber = Int(secondText.text!){
+            
+                result = firstNumber / secondNumber
+                
+                resultText.text = String(result)
+            }
+        }
+    }
+    
 }
 
